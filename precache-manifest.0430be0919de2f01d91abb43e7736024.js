c45c05444355e9eb5a6dbac0839ebab5c31ce649
vadikmash <vadikmash@gmail.com>
2019-11-29 02:15:08 +0300

    init: init gh-pages

---
+++ precache-manifest.0430be0919de2f01d91abb43e7736024.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9239a9d0b0b5c0d42a39812faa7b1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b9a9f6da299e5c4e955",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "36e887daa5b82c36cc7b",
+    "url": "/static/js/2.f5ce9da2.chunk.js"
+  },
+  {
+    "revision": "7b9a9f6da299e5c4e955",
+    "url": "/static/js/main.4bf4acd6.chunk.js"
+  },
+  {
+    "revision": "b3dec2523adbeb99b330",
+    "url": "/static/js/runtime-main.e19b0c5f.js"
+  }
+]);
